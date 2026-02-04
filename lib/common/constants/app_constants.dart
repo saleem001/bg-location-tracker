4febf179245e35afdff547fe58da4fda9a03326a
Muhammad Saleem <saleemarshad001@gmail.com>
2026-02-04 19:51:24 +0500

    added geofence

---
+++ lib/common/constants/app_constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  //Geofence Actions
+  static const String geofenceActionEnter = "ENTER";
+  static const String geofenceActionExit = "EXIT";
+  static const String geofenceActionDwell = "DWELL";
+}
